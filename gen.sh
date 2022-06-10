dc1d4032ee119bd2636b04258d4b2df867c4ea40
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-10 11:30:07 +0700

    remove

--- gen.sh
+++ gen.sh
@@ -8,7 +8,7 @@ echo $(for each in $(seq 2 40); do printf "a"; done) > ./ex01/testShell00
 touch -a -m -t 202406012342.00 ./ex01/testShell00
 chmod u=r,go=rx ./ex01/testShell00
 tar -cf ./ex01/testShell00.tar ./ex01/*
-#rm -r ./ex01 testShell00
+rm -r ./ex01 testShell00
 
 #ex02
 mkdir ex02
